a93f693e3b310fd12a442b497c65edac95ccd986
Kyedonghwan <61385046+Kyedonghwan@users.noreply.github.com>
2020-09-15 21:10:40 +0900

    20150915-3

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
+app.set("view engine", "pug"); // view engine이 보여질 확장자가 pug임을 명시.
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
