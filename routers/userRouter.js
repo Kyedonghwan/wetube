53121ad6ef975e475a8dfbe9daf6c5b7114cd0a4
Kyedonghwan <61385046+Kyedonghwan@users.noreply.github.com>
2020-09-15 19:18:27 +0900

    20200915-2

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,17 @@
 import express from "express";
+import {
+  userDetail,
+  editProfile,
+  changePassword,
+  users,
+} from "../controllers/userController";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
